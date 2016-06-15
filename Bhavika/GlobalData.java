835738d4421355cbf9d83f13a77671045766a797
bhavikap271 <bhavikap@buffalo.edu>
2016-06-14 21:18:36 -0400

    Add files via upload

---
+++ Bhavika/GlobalData.java
@@ -0,0 +1,12 @@
+package cse.buffalo.edu.swing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlobalData{
+	
+	static List<String> allTables = new ArrayList<String>();
+	
+
+
+}
