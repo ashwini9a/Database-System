725465b6ef5df89388f6ba6bae225f914d4e28be
fenqin <fenqin@buffalo.edu>
2016-06-16 20:53:01 -0400

    Fixed insert tuples

--- Combined/DB_Project1/src/Screens/OperationFunctions.java
+++ Combined/DB_Project1/src/Screens/OperationFunctions.java
@@ -17,46 +17,54 @@ import org.json.simple.parser.ParseException;
 
 public class OperationFunctions {
 	static String[] colnmI;
-	public static void insertInTable(String tnm,JSONObject json)
-	{
-		
+
+	public static void insertInTable(String tnm, JSONObject json) {
+
 		JSONParser parser = new JSONParser();
 		try {
-			
-			Object obj = parser.parse(new FileReader("Data/Records/"+tnm+".json"));
+
+			Object obj = parser.parse(new FileReader("Data/Records/" + tnm + ".json"));
 			JSONObject json1 = (JSONObject) obj;
-			//System.out.println("here3");
-			
-			JSONArray headers = (JSONArray) json1.get("Records");
-		
 			System.out.println(json1.toJSONString());
-			File file = new File("Data/Records/"+tnm+".json");
+
+			try {
+				JSONArray headers = (JSONArray) json1.get("Records");
+				headers.add(json);
+				json1.put("Records", headers);
+			} catch (ClassCastException e) {
+				JSONArray JA = new JSONArray();
+				JA.add(json);
+				json1.put("Records", JA);
+			}
+			
+			
+			System.out.println(json1.toJSONString());
+
+			File file = new File("Data/Records/" + tnm + ".json");
 			FileWriter fw = null;
 			BufferedWriter bw = null;
 			fw = new FileWriter(file.getAbsoluteFile());
 			bw = new BufferedWriter(fw);
-			
-		      
-		      bw.write(json1.toJSONString());
-		      bw.flush();
-		      bw.close();
-			
+
+			bw.write(json1.toJSONString());
+			bw.flush();
+			bw.close();
+
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
 		}
 	}
-	public static void updateInTable(String tnm,JSONObject json)
-	{
-		
-	}
-	public static void deleteInTable(String tnm,JSONObject json)
-	{
-		
-	}
-	public static void searchInTable(String tnm,JSONObject json)
-	{
-		
+
+	public static void updateInTable(String tnm, JSONObject json) {
+
 	}
 
+	public static void deleteInTable(String tnm, JSONObject json) {
+
+	}
+
+	public static void searchInTable(String tnm, JSONObject json) {
+
+	}
 }
