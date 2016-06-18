e5bd533d538a0d921c5daa30f08c0bd2b6792910
fenqin <fenqin@buffalo.edu>
2016-06-17 23:18:37 -0400

    Projection is now functional!

---
+++ Combined/DB_Project1/src/Screens/ProjectionRecords.java
@@ -0,0 +1,232 @@
+package Screens;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class ProjectionRecords {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void projectRecords(String tableName, ArrayList<String> SelectedAttributes) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+
+					ProjectionRecords window = new ProjectionRecords();
+					System.out.println(tableName);
+
+					// populate records in the table
+					window.frame.setTitle(tableName);
+					window.populateRecords("Data/Records/" + tableName + ".json", SelectedAttributes);
+
+					// display all the records of this table
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public static void projectRecords(String tableName) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+
+					ProjectionRecords window = new ProjectionRecords();
+					System.out.println(tableName);
+
+					// populate records in the table
+					window.frame.setTitle(tableName);
+					window.populateRecords("Data/Records/" + tableName + ".json");
+
+					// display all the records of this table
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public void populateRecords(String name, ArrayList<String> SelectedAttributes) {
+		JSONParser parser = new JSONParser();
+		try {
+			Object obj = parser.parse(new FileReader(name));
+			JSONObject json = (JSONObject) obj;
+			JSONArray headers = (JSONArray) json.get("Records");
+
+			if (headers.size() == 0) {
+				JOptionPane.showMessageDialog(null, "No Records to Display", "Warning",
+						JOptionPane.INFORMATION_MESSAGE);
+			} else {
+
+				String[] columnNames = new String[SelectedAttributes.size()];
+				SelectedAttributes.toArray(columnNames);
+				
+				table = new JTable();
+				table.setModel(new DefaultTableModel(new Object[][] {}, columnNames) {
+
+					@Override
+					public boolean isCellEditable(int row, int col) {
+
+						return false;
+					}
+
+				});
+
+				DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
+
+				for (int i = 0; i < headers.size(); i++) {
+
+					Object temp = parser.parse(headers.get(i).toString());
+					JSONObject currJson = (JSONObject) temp;
+
+					Object[] data = new Object[columnNames.length];
+					int index = 0;
+					for (String key : columnNames) {
+						data[index] = currJson.get(key);
+						index++;
+					}
+					tableModel.addRow(data);
+				}
+
+				table.setFillsViewportHeight(true);
+				JScrollPane scrollPane = new JScrollPane(table);
+				scrollPane.setBounds(37, 5, 468, 100);
+				scrollPane.setViewportView(table);
+				scrollPane.setPreferredSize(new Dimension(468, 100));
+				frame.getContentPane().add(scrollPane);
+
+				this.frame.setVisible(true);
+
+				// frame.pack();
+			}
+
+		} catch (FileNotFoundException e) {
+			// System.out.println("FileNotFoundException");
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void populateRecords(String name) {
+		JSONParser parser = new JSONParser();
+		try {
+
+			Object obj = parser.parse(new FileReader(name));
+			JSONObject json = (JSONObject) obj;
+			JSONArray headers = (JSONArray) json.get("Records");
+			System.out.println(headers.toString());
+
+			if (headers.size() == 0) {
+
+				JOptionPane.showMessageDialog(null, "No Records to Display", "Warning",
+						JOptionPane.INFORMATION_MESSAGE);
+
+			} else {
+
+				Object temp = parser.parse(headers.get(0).toString());
+				JSONObject currJson = (JSONObject) temp;
+				System.out.println("test!!!!" + headers.get(0).toString());
+				Set<String> keys = currJson.keySet();
+				String[] columnNames = keys.toArray(new String[keys.size()]);
+				for (int i = 0; i < columnNames.length; i++)
+					System.out.println(i+"th:" + columnNames[i]);
+
+				table = new JTable();
+
+				table.setModel(new DefaultTableModel(new Object[][] {}, columnNames) {
+
+					@Override
+					public boolean isCellEditable(int row, int col) {
+						return false;
+					}
+				});
+
+				DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
+
+				for (int i = 0; i < headers.size(); i++) {
+					temp = parser.parse(headers.get(i).toString());
+					currJson = (JSONObject) temp;
+
+					Object[] data = new Object[columnNames.length];
+					int index = 0;
+					for (String key : keys) {
+						data[index] = currJson.get(key);
+						index++;
+					}
+					tableModel.addRow(data);
+				}
+
+				table.setFillsViewportHeight(true);
+				JScrollPane scrollPane = new JScrollPane(table);
+				scrollPane.setBounds(37, 5, 468, 100);
+				scrollPane.setViewportView(table);
+				scrollPane.setPreferredSize(new Dimension(468, 100));
+				frame.getContentPane().add(scrollPane);
+
+				this.frame.setVisible(true);
+
+				// frame.pack();
+			}
+
+		} catch (FileNotFoundException e) {
+			// System.out.println("FileNotFoundException");
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ProjectionRecords() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 800, 300);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		frame = new JFrame();
+		frame.setBounds(100, 100, 800, 300);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+}
--- Combined/DB_Project1/src/Screens/SelectAtt.java
+++ Combined/DB_Project1/src/Screens/SelectAtt.java
@@ -2,14 +2,19 @@ package Screens;
 
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
 
 import javax.swing.DefaultCellEditor;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -20,16 +25,19 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumnModel;
+import javax.swing.JButton;
 
 public class SelectAtt extends JFrame {
 
 	private JTable table;
+	private String[] columnNames;
+	private ArrayList<String> selectedAtts;
 
 	/**
 	 * Create the application.
 	 */
-	public SelectAtt(String tableName){
-		
+	public SelectAtt(String tableName) {
+
 		this.setBounds(100, 100, 500, 300);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.getContentPane().setLayout(null);
@@ -41,20 +49,20 @@ public class SelectAtt extends JFrame {
 			Object obj = parser.parse(new FileReader("Data/MetaData/" + tableName + ".json"));
 			JSONObject json = (JSONObject) obj;
 			JSONArray headers = (JSONArray) json.get("headers");
-			//System.out.println(headers.toString());
+			// System.out.println(headers.toString());
 
 			Object temp = parser.parse(headers.get(0).toString());
 			JSONObject currJson = (JSONObject) temp;
 			Set<String> keys = currJson.keySet();
 			String[] tempArr = keys.toArray(new String[keys.size()]);
 
-			String[] columnNames = new String[tempArr.length + 1];
-			for (int i = 0; i < tempArr.length; i++){
-				   columnNames[i] = tempArr[i];
+			columnNames = new String[tempArr.length + 1];
+			for (int i = 0; i < tempArr.length; i++) {
+				columnNames[i] = tempArr[i];
 			}
-			
+
 			columnNames[tempArr.length] = "include?";
-			
+
 			table = new JTable();
 			table.setModel(new DefaultTableModel(new Object[][] {}, columnNames) {
 				@Override
@@ -64,21 +72,16 @@ public class SelectAtt extends JFrame {
 					else
 						return false;
 				}
-				
-				public Class<?> getColumnClass(int columnIndex)
-		         {
-					if(columnIndex == 3)
-						 return Boolean.class;
-					
+
+				public Class<?> getColumnClass(int columnIndex) {
+					if (columnIndex == columnNames.length - 1)
+						return Boolean.class;
+
 					return String.class;
-		             
-		         }
+
+				}
 			});
 
-			//table.getColumn("include?").setCellEditor(new DefaultCellEditor(new JCheckBox()));
-			
-			//table.setEditingColumn(columnNames.length);
-			//TableColumnModel tcm = table.getColumnModel();
 			DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
 
 			for (int i = 0; i < headers.size(); i++) {
@@ -91,9 +94,9 @@ public class SelectAtt extends JFrame {
 					data[index] = currJson.get(key);
 					index++;
 				}
-				
-				data[tempArr.length] =  Boolean.FALSE;
-				
+
+				data[tempArr.length] = Boolean.FALSE;
+
 				tableModel.addRow(data);
 			}
 
@@ -111,6 +114,45 @@ public class SelectAtt extends JFrame {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+
+		JButton btnAll = new JButton("Project All Attributes");
+		btnAll.setBounds(312, 233, 162, 30);
+		btnAll.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ProjectionRecords display = new ProjectionRecords();
+				display.projectRecords(tableName);
+			}
+		});
+
+		getContentPane().add(btnAll);
+
+		JButton btnSelected = new JButton("Project Select Attributes");
+		btnSelected.setBounds(117, 234, 187, 29);
+		btnSelected.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				selectedAtts = new ArrayList<String>();
+				// DefaultTableModel dm = (DefaultTableModel) table.getModel();
+				for (int i = 0; i < table.getRowCount(); i++) {
+					Boolean isChecked = Boolean.valueOf(table.getValueAt(i, columnNames.length - 1).toString());
+
+					if (isChecked) {
+						// this may be cause problem later because 3 is fixed
+						// number
+						selectedAtts.add((String) table.getValueAt(i, 3));
+					} else {
+						//
+					}
+				}
+				if (selectedAtts.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Please select at least one attribute!", "Error", JOptionPane.ERROR_MESSAGE);
+				} else {
+					ProjectionRecords display = new ProjectionRecords();
+					display.projectRecords(tableName, selectedAtts);
+				}
+			}
+		});
+
+		getContentPane().add(btnSelected);
 
 		this.setVisible(true);
 	}
