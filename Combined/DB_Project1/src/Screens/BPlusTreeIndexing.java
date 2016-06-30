85a0cbe5ac909e32b2fc93a2f9092030da10ad4d
fenqin <fenqin@buffalo.edu>
2016-06-30 00:08:53 -0400

    Fixed the write index and finished load index file

--- Combined/DB_Project1/src/Screens/BPlusTreeIndexing.java
+++ Combined/DB_Project1/src/Screens/BPlusTreeIndexing.java
@@ -9,6 +9,7 @@ import java.awt.Dimension;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -41,6 +42,15 @@ import jdbm.btree.BTree;
  * @version $Id: FamousPeople.java,v 1.6 2003/10/21 15:32:02 boisvert Exp $
  */
 public class BPlusTreeIndexing {
+	RecordManager recman;
+	long recid;
+	Properties props = new Properties();
+	TupleBrowser browser;
+	Tuple tuple = new Tuple();
+	BTree tree = null;
+	String TABLE_NAME;
+	String BTREE_NAME;
+	JSONParser parser = new JSONParser();
 
 	public BPlusTreeIndexing() {
 
@@ -49,16 +59,8 @@ public class BPlusTreeIndexing {
 	// public BPlusTreeIndexing(JSONArray headers, String table_name, String
 	// att) {
 	public BTree GetBPlusTreeIndexing(JSONArray headers, String table_name, String att) {
-		RecordManager recman;
-		long recid;
-		Properties props = new Properties();
-		TupleBrowser browser;
-		Tuple tuple = new Tuple();
-		BTree tree = null;
-		String TABLE_NAME = table_name;
-		String BTREE_NAME = table_name + "." + att;
-		JSONParser parser = new JSONParser();
-
+		TABLE_NAME = table_name;
+		BTREE_NAME = table_name + "_" + att;
 		try {
 			// open database and setup an object cache
 			Object[] db = new Object[headers.size()];
@@ -95,12 +97,12 @@ public class BPlusTreeIndexing {
 		return tree;
 	}
 
-	public void SaveBTree(JSONArray headers, BTree tree, String tableName) {
+	public void SaveBTree(JSONArray headers, BTree tree, String tableName, String att) {
 		try {
 			TupleBrowser browser = tree.browse();
 			Tuple tuple = new Tuple();
 			JSONObject temp;
-			File file = new File("Data/Index/" + tableName + ".json");
+			File file = new File("Data/Index/" + tableName + "_" + att + ".json");
 			FileWriter fw = null;
 			BufferedWriter bw = null;
 
@@ -115,12 +117,13 @@ public class BPlusTreeIndexing {
 			}
 
 			String records = "{\"index\":[";
-
 			while (browser.getNext(tuple)) {
 				Object key1 = tuple.getKey();
 				int i = headers.indexOf((JSONObject) tuple.getValue());
 				JSONObject json = new JSONObject();
-				json.put(key1, i);
+				json.put(att, key1);
+				json.put("row #", i);
+
 				records += json.toString() + ",";
 				;
 				System.out.println("save test!!!" + "position:" + "i=" + i + " key=" + key1);
@@ -155,7 +158,57 @@ public class BPlusTreeIndexing {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
+	public BTree LoadBTree(JSONArray headers, String tableName, String att) {
+		try {
+			// load the index file
+			FileReader f1 = new FileReader("Data/Index/" + tableName + "_" + att + ".json");
+			Object obj = parser.parse(f1);
+			JSONObject json = (JSONObject) obj;
+			JSONArray index = (JSONArray) json.get("index");
+
+			if (index.size() != headers.size()) {
+				JOptionPane.showMessageDialog(null, "records number from index file and data file not match!", "Error",
+						JOptionPane.ERROR_MESSAGE);
+			}
+
+			// read the key value of index file
+			Object[] db = new Object[index.size()];
+			for (int i = 0; i < index.size(); i++) {
+				JSONObject currJson = (JSONObject) parser.parse(index.get(i).toString());
+				// !!!!!!!!!!!!!!!!!!
+				// This cast may have problem
+				db[i] = currJson.get(att);
+				System.out.println("test!!" + db[i]);
+			}
+
+			// get the tuple address from the row number in the index file
+			JSONObject[] tupleaddress = new JSONObject[index.size()];
+			for (int i = 0; i < index.size(); i++) {
+				JSONObject currJson = (JSONObject) parser.parse(index.get(i).toString());
+				//we need this weird cast to convert long to int
+				Long temp = (Long) currJson.get("row #");
+				int j = temp.intValue();
+				// !!!!!!!!!!!!!!!!!!
+				// This cast may have problem
+				tupleaddress[i] = (JSONObject) headers.get(j);
+			}
+
+			recman = RecordManagerFactory.createRecordManager(TABLE_NAME, props);
+			tree = BTree.createInstance(recman, new StringComparator());
+			recman.setNamedObject(att, tree.getRecid());
+
+			System.out.println("Created a new empty BTree");
+			System.out.println();
+			for (int i = 0; i < db.length; i++) {
+				System.out.println("Insert: " + db[i]);
+				tree.insert(db[i], tupleaddress[i], false);
+			}
+
+		} catch (Exception except) {
+			except.printStackTrace();
+		}
+		return tree;
+	}
 }
--- Combined/DB_Project1/src/Screens/SelectAtt.java
+++ Combined/DB_Project1/src/Screens/SelectAtt.java
@@ -164,8 +164,9 @@ public class SelectAtt extends JFrame {
 					JSONObject json = (JSONObject) obj;
 					JSONArray headers = (JSONArray) json.get("Records");
 					System.out.println(headers.get(0).toString());
+					String att = "Name";
 					BPlusTreeIndexing indexing = new BPlusTreeIndexing();
-					BTree tree = indexing.GetBPlusTreeIndexing(headers, tableName, "Name");
+					BTree tree = indexing.GetBPlusTreeIndexing(headers, tableName, att);
 
 					// traverse people in order
 					TupleBrowser browser = tree.browse();
@@ -175,12 +176,13 @@ public class SelectAtt extends JFrame {
 						System.out.println(obj1);
 					}
 					JSONObject test = (JSONObject) tree.find("George");
-					tree.insert("George2", test, false);
+					
+					//tree.insert("George2", test, false);		//this is tested
 					// traverse people in reverse order
 					System.out.println("test 1!!!!");
 					System.out.println(test==headers.get(1));
 
-					System.out.println("test 2!!!!"+test.get("Name"));
+					System.out.println("test 2!!!!"+test.get(att));
 
 					System.out.println("Reverse order:");
 					browser = tree.browse(null); // position browser at end of
@@ -192,7 +194,37 @@ public class SelectAtt extends JFrame {
 					}
 					
 					System.out.println("Save:");
-					indexing.SaveBTree(headers, tree, tableName);
+					indexing.SaveBTree(headers, tree, tableName,att);
+					
+					//below is the test for tree load from saved index file
+					System.out.println("!!!!");
+					System.out.println("below is the test for tree load from saved index file");
+					BTree tree2 = indexing.LoadBTree(headers, tableName, att);
+					
+					TupleBrowser browser2 = tree2.browse();
+					Tuple tuple2 = new Tuple();
+					while (browser.getNext(tuple2)) {
+						Object obj1 = tuple2.getKey();
+						System.out.println(obj1);
+					}
+					JSONObject test2 = (JSONObject) tree2.find("George");
+					
+					//tree.insert("George2", test, false);		//this is tested
+					// traverse people in reverse order
+					System.out.println("test 1!!!!");
+					System.out.println(test2==headers.get(1));
+
+					System.out.println("test 2!!!!"+test2.get(att));
+
+					System.out.println("Reverse order:");
+					browser = tree.browse(null); // position browser at end of
+													// the list
+
+					while (browser.getPrevious(tuple2)) {
+						Object obj1 = tuple2.getKey();
+						System.out.println(obj1);
+					}
+					
 					
 				} catch (FileNotFoundException ex) {
 					ex.printStackTrace();
